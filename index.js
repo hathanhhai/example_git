bf8725a03455b5b354f4a6d993f029c294d52c74
hathanhhai <thanhhaih23@gmail.com>
2022-03-17 19:59:46 +0700

    3

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const port = 8031
+var cors = require('cors')
+app.use(cors())
+
+app.get('/', (req, res) => {
+  return res.send("nodejs")
+})
+app.get('/color', (req, res) => {
+    res.send([
+    {
+      color: "red",
+      value: "#f00"
+    },
+    {
+      color: "green",
+      value: "#0f0"
+    },
+    {
+      color: "blue",
+      value: "#00f"
+    },
+    {
+      color: "cyan",
+      value: "#0ff"
+    },
+    {
+      color: "magenta",
+      value: "#f0f"
+    },
+    {
+      color: "yellow",
+      value: "#ff0"
+    },
+    {
+      color: "black",
+      value: "#000"
+    }
+  ])
+  res.send("32323")
+  console.log("333")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
